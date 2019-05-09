02a5b1a2bef100ddb6eab0188b4b0beda6f143d3
Thanakrit Wongsatit <austinwongsatit@gmail.com>
2019-05-09 11:23:59 +0700

    Add Histogram

--- File1.py
+++ File1.py
@@ -4,6 +4,12 @@ Created on Thu May  9 10:39:15 2019
 
 @author: azuromalachite
 """
-import matplotlib.pyplot as plt
 
-print("This is the end of the world !!!!!!!!!")
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("This is the end of the world")
+
+plt.hist(np.array([1,1,2,3,4,3,5,5,5,6,7,8,8,8]))
+plt.show()
+
